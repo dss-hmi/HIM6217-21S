261026cabd3827d7deb24832ebd3d7a5da5b5226
Andriy V. Koval <koval.andrey@gmail.com>
2021-01-06 19:51:23 -0500

    Create sqlbolt-ellis.R
    seems, that this really should be a ferry, not ellis

---
+++ manipulation/sqlbolt/sqlbolt-ellis.R
@@ -0,0 +1,172 @@
+rm(list = ls(all.names = TRUE)) # Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below. 
+# ---- load-packages -----------------------------------------------------------
+# Import only certain functions of a package into the search path.
+import::from("magrittr", "%>%")
+
+requireNamespace("readr"        )
+requireNamespace("tidyr"        )
+requireNamespace("dplyr"        ) 
+requireNamespace("rlang"        ) # Language constructs, like quosures
+requireNamespace("checkmate"    ) # For asserting conditions meet expected patterns
+requireNamespace("DBI"          ) # Database-agnostic interface
+requireNamespace("RSQLite"      ) # Lightweight database for non-PHI data.
+requireNamespace("OuhscMunge"   ) # remotes::install_github(repo="OuhscBbmc/OuhscMunge")
+library(dplyr)
+
+# ---- declare-globals ---------------------------------------------------------
+# Constant values that won't change.
+
+path_db_movies     <- "data-public/derived/sqlbolt-movies-db.sqlite3"
+path_db_employees  <- "data-public/derived/sqlbolt-employees-db.sqlite3"
+
+path_movies    <- "./data-public/raw/sqlbolt/movies.csv"
+path_boxoffice <- "./data-public/raw/sqlbolt/boxoffice.csv"
+path_buildings <- "./data-public/raw/sqlbolt/buildings.csv"
+path_employees <- "./data-public/raw/sqlbolt/employees.csv"
+
+
+
+# Execute to specify the column types.  
+col_types_movies <- 
+  readr::cols_only(
+    `Id`             = readr::col_integer(),
+    `Title`          = readr::col_character(),
+    `Director`       = readr::col_character(),
+    `Year`           = readr::col_integer(),
+    `Length_minutes` = readr::col_integer()
+
+)
+col_types_boxoffice <- 
+  readr::cols_only(
+    `Movie_id`             = readr::col_integer(),
+    `Rating`               = readr::col_double(),
+    `Domestic_sales`       = readr::col_integer(),
+    `International_sales`  = readr::col_integer()
+)
+
+col_types_buildings <- 
+  readr::cols_only(
+    `Building_name` = readr::col_character(),
+    `Capacity`      = readr::col_integer()
+  )
+
+col_types_employees <- 
+  readr::cols_only(
+    `Role`             = readr::col_character(),
+    `Name`               = readr::col_character(),
+    `Building`       = readr::col_character(),
+    `Years_employed`  = readr::col_integer()
+  )
+
+
+
+
+# ---- load-data ---------------------------------------------------------------
+# Read the CSVs
+ds_movies <- readr::read_csv(path_movies , col_types=col_types_movies)
+ds_boxoffice  <- readr::read_csv(path_boxoffice , col_types=col_types_boxoffice)
+
+ds_buildings <- readr::read_csv(path_buildings , col_types=col_types_buildings)
+ds_employees <- readr::read_csv(path_employees , col_types=col_types_employees)
+
+
+rm(col_types_movies,col_types_boxoffice,col_types_buildings,col_types_employees)
+
+# ---- tweak-data --------------------------------------------------------------
+# OuhscMunge::column_rename_headstart(ds) # Help write `dplyr::select()` call.
+ds <-
+  ds %>%
+  dplyr::select(    # `dplyr::select()` drops columns not included.
+    county_id,
+    county_name,
+    category,
+    time_zone         = `timezone`,
+    desired,
+  ) %>%
+  dplyr::filter(desired) %>%
+  dplyr::mutate(
+
+  ) # %>%
+  # dplyr::arrange(subject_id) # %>%
+  # tibble::rowid_to_column("subject_id") # Add a unique index if necessary
+
+# ---- verify-values -----------------------------------------------------------
+# OuhscMunge::verify_value_headstart(ds)
+checkmate::assert_integer(  ds$county_id   , any.missing=F , lower=51, upper=72 , unique=T)
+checkmate::assert_character(ds$county_name , any.missing=F , pattern="^.{5,25}$", unique=T)
+checkmate::assert_character(ds$category    , any.missing=F , pattern="^.{5,10}$" )
+checkmate::assert_character(ds$time_zone   , any.missing=F , pattern="^.{5,25}$" )
+checkmate::assert_logical(  ds$desired     , any.missing=F                       )
+
+# ---- specify-columns-to-upload -----------------------------------------------
+# Print colnames that `dplyr::select()`  should contain below:
+#   cat(paste0("    ", colnames(ds), collapse=",\n"))
+
+# Define the subset of columns that will be needed in the analyses.
+#   The fewer columns that are exported, the fewer things that can break downstream.
+
+ds_slim <-
+  ds %>%
+  # dplyr::slice(1:100) %>%
+  dplyr::select(
+    county_id,
+    county_name,
+    category,
+    time_zone,
+    # desired
+  )
+
+
+# ---- save-to-db --------------------------------------------------------------
+# If a database already exists, this single function uploads to a SQL Server database.
+# OuhscMunge::upload_sqls_odbc(
+#   d             = ds_slim,
+#   schema_name   = "skeleton",         # Or config$schema_name,
+#   table_name    = "dim_county",
+#   dsn_name      = "skeleton-example", # Or config$dsn_qqqqq,
+#   timezone      = config$time_zone_local, # Uncomment if uploading non-UTC datetimes
+#   clear_table   = T,
+#   create_table  = F
+# ) # 0.012 minutes
+
+
+# If there's no PHI, a local database like SQLite fits a nice niche if
+#   * the data is relational and
+#   * later, only portions need to be queried/retrieved at a time (b/c everything won't need to be loaded into R's memory)
+# cat(dput(colnames(ds)), sep = "\n")
+sql_create <- c(
+  "
+    DROP TABLE IF EXISTS dim_county;
+  ",
+  "
+    CREATE TABLE `dim_county` (
+      county_id               int         not null primary key,
+      county_name             varchar(25) not null,
+      category                varchar(10) not null,
+      time_zone               varchar(25) not null
+    );
+  "
+)
+
+# Remove old DB
+# if( file.exists(path_db) ) file.remove(path_db)
+
+# Open connection
+cnn <- DBI::dbConnect(drv=RSQLite::SQLite(), dbname=path_db)
+result <- DBI::dbSendQuery(cnn, "PRAGMA foreign_keys=ON;") #This needs to be activated each time a connection is made. #http://stackoverflow.com/questions/15301643/sqlite3-forgets-to-use-foreign-keys
+DBI::dbClearResult(result)
+DBI::dbListTables(cnn)
+
+# Create tables
+sql_create %>%
+  purrr::walk(~DBI::dbExecute(cnn, .))
+DBI::dbListTables(cnn)
+
+# Write to database
+DBI::dbWriteTable(cnn, name='dim_county',            value=ds_slim,        append=TRUE, row.names=FALSE)
+
+# Close connection
+DBI::dbDisconnect(cnn)
