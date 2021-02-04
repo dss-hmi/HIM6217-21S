454d5441e981c15093cf3f77b0caaef51dacff13
Andriy V. Koval <koval.andrey@gmail.com>
2021-02-04 11:48:10 -0500

    rough draft finished

--- homework/homework-3-andriy.sql
+++ homework/homework-3-andriy.sql
@@ -2,33 +2,78 @@
 -- All questions target the database `synpuf_2`
 -- Note 1: all patients found in `dx` and `visit` can be found in `patient`
 -- Note 2: Some patients from `patient` are missing from `dx`, `visit`, or both  
-
--- Requirements:
--- Must use: count(), distinct(), WHERE, LIKE
--- Must NOT use: 
--- Output dimensions: 1x1
--- Output must contain columns: n_contain_pain
-
-
---  1)
-
-;
+-- Note 3: When asking about the "number of patients" it is implied that patients are unique
+-- Note 4: the "MUST use" requirement may not contain ALL the necessary keywords to complete the task
+-- Note 5: If not explicitly forbidden in "must NOT use" requirement, any keywords can be used
+-- Note 5: The uniqueness of the diagnosis is defined by its icd9_code, not icd9_description (e.g. 234.9 and 234.99 are not the same)
 
 -- PART I
 
--- How many unique patients can be observed in the `patient` table? 
+-- 1) How many patients can be observed in the `patient` table? 
 -- Output dimensions: 1x1
+-- Output must contain columns: `person_count`
 SELECT 
 count(distinct(person_id)) as person_count
 FROM patient
 ;
 
 
+-- 2) How many patients present in `dx` table are missing from `visit` table?
+-- MUST use: IS NULL
+-- Output dimensions: 1x1
+-- Output must contain columns: `person_count`
+SELECT
+  count(distinct dx.person_id) as person_count
+  --distinct dx.person_id -- to view id values
+FROM  dx    
+  left  join visit on dx.person_id = visit.person_id
+WHERE visit.visit_id is null
+;
 
--- How many patients are present in all three tables?
+-- 3) How many patients present in both `patient` and `dx` tables are missing from `visit` table?
+-- MUST use: WHERE, LEFT JOIN, INNER JOIN
+-- Output dimensions: 1x1
+-- Output must contain columns: `person_count`
+SELECT
+  count(distinct p.person_id) as person_count
+  --distinct p.person_id -- to view id values
+FROM patient p
+  inner join dx    as d on p.person_id = d.person_id
+  left  join visit as v on p.person_id = v.person_id
+WHERE v.visit_id is null
+;
+
+
+-- 4) How many patients present in `visit` table are missing from `dx` table?
+-- Output dimensions: 1x1
+-- Output must contain columns: `person_count`
+SELECT
+  count(distinct visit.person_id) as person_count
+  --distinct visit.person_id -- to see why filtering on dx.dx_id works
+  --,dx.dx_id
+FROM  visit    
+  left  join dx on visit.person_id = dx.person_id
+WHERE dx.dx_id is null -- why does this work?
+;
+
+-- 5) How many unique patients present in both `patient` and `visit` tables are missing from `dx` table?
+-- Output dimensions: 1x1
+-- Output must contain columns: `person_count`
+SELECT
+  count(distinct
+  p.person_id) as patient_count
+  --*
+FROM patient p
+  left  join dx    as d on p.person_id = d.person_id
+  inner join visit as v on p.person_id = v.person_id
+WHERE d.dx_id is null
+;
+
+-- 6) How many patients are present in all three tables?
 -- MUST use: LEFT JOIN
 -- must NOT use: INNER JOIN
 -- Output dimensions: 1x1
+-- Output must contain columns: `person_count`
 SELECT 
  count(distinct p.person_id) as person_count
 FROM patient p
@@ -41,49 +86,11 @@ WHERE
 ;
 
 
--- How many unique patients present in `dx` table but missing in `visit` table?
--- MUST use: IS NULL
-SELECT
-  count(distinct dx.person_id) as patient_count
-  --distinct dx.person_id
-FROM  dx    
-  left  join visit on dx.person_id = visit.person_id
-WHERE visit.visit_id is null;
-
--- How many unique patients present in both `patient` and `dx` tables but absent from `visit` table?
--- MUST use: WHERE, LEFT JOIN, INNER JOIN,
-SELECT
-  --distinct p.person_id
-  count(distinct p.person_id) as patient_count
-FROM patient p
-  inner join dx    as d on p.person_id = d.person_id
-  left  join visit as v on p.person_id = v.person_id
-WHERE v.visit_id is null;
-
-
--- How many unique patients are present in `visit` table but absent from `dx` table?
--- MUST use: LEFT JOIN, WHERE, IS NULL
-SELECT
-  count(distinct visit.person_id) as patient_count
-  --distinct visit.person_id -- to see why filtering on dx.dx_id works
-  --,dx.dx_id
-FROM  visit    
-  left  join dx on visit.person_id = dx.person_id
-WHERE dx.dx_id is null -- why does this work?
-;
-
--- How many unique patients are present in both `patient` and `visit` tables but absent from `ds` table?
-SELECT
-  count(distinct
-  p.person_id) as patient_count
-  --*
-FROM patient p
-  left  join dx    as d on p.person_id = d.person_id
-  inner join visit as v on p.person_id = v.person_id
-WHERE d.dx_id is null
-;
-
--- How many persons in `patient` table are missing from both `dx` and `visit` tables? 
+-- 7) How many persons in `patient` table are missing from both `dx` and `visit` tables? 
+-- MUST use: LEFT JOIN
+-- must NOT use: INNER JOIN
+-- Output dimensions: 1x1
+-- Output must contain columns: `person_count`
 SELECT
   -- distinct patient.person_id, dx.dx_id, visit.visit_id
   count(distinct patient.person_id) as person_count
@@ -99,8 +106,77 @@ WHERE
 
 -- PART II
 
+-- 8) Among  patients in `dx` table who are missing from `visit` table, what `person_id` number is the smallest?
+-- Write the query that lists patients present in `dx` but NOT in `visit` table
+-- Sort by `person_id` found in the `visit` table
+-- Output dimensions: ?x2
+-- Output must include columns named: `person_id_dx`, `person_id_visit`
+select 
+distinct dx.person_id as person_id_dx
+, visit.person_id as person_id_visit
+from dx
+left join visit on dx.person_id = visit.person_id
+order by person_id_visit
+;
 
--- Speaking of patients observed in the `visit` table, 
+
+-- 9) Among patients in `visit` table who are missing from `dx` table, what `person_id` number is the smallest?
+-- Hint 1: Write the query that lists patients present in `visit` but NOT in `dx` table
+-- Hint 2: Sort by person_id found in the `dx` table
+-- Output dimensions: ?x2
+-- Output must include columns named: `person_id_visit`, `person_id_dx`
+select 
+distinct visit.person_id as person_id_visit
+, dx.person_id as person_id_dx
+from visit
+left join dx on visit.person_id = dx.person_id
+order by person_id_dx
+;
+
+-- 10) Among patients missing from both `dx` and `visit` tables, what person_id number is the smallest?
+-- Output dimensions: ?x3
+-- Output must include columns named: `person_id_patient`, `dx_id`, `visit_id`
+SELECT
+  distinct patient.person_id as person_id_patient, dx.dx_id, visit.visit_id
+  --count(distinct patient.person_id) as person_count
+FROM patient 
+  left  join dx    on patient.person_id = dx.person_id
+  left  join visit on patient.person_id = visit.person_id
+WHERE 
+  dx.dx_id   is null
+  and
+  visit.visit_id is null
+ORDER BY patient.person_id asc
+;
+
+
+-- 11) Among patients present in all three tables, what person_id number is the smallest?
+SELECT
+  distinct patient.person_id as person_id_patient,
+  dx.person_id as person_id_dx, visit.person_id as person_id_visit
+  --count(distinct patient.person_id) as person_count
+FROM patient 
+  inner  join dx    on patient.person_id = dx.person_id
+  inner  join visit on patient.person_id = visit.person_id
+ORDER BY patient.person_id asc
+;
+
+
+-- PART III
+
+-- 12) What is the difference (in absolute value) between the number of persons in `patient` table
+-- and the number of persons in the `dx` table?
+-- Output dimensions: 1x3
+-- Output must contain columns: `n_person_patient`, `n_person_dx`, `n_diff`
+SELECT 
+count(distinct(a.person_id)) as person_count_patient,
+count(distinct(b.person_id)) as person_count_dx,
+abs(count(distinct(b.person_id)) - count(distinct(a.person_id))) as difference_count
+FROM patient as a
+LEFT join dx as b
+;
+
+-- 13) Speaking of patients observed in the `visit` table, 
 -- What is the average number of visits per person? (rounded down to the nearest integer)
 -- Output dimensions: 1x3
 -- Output must include columns named `person_count`, `visit_count`, `visit_mean`
@@ -111,11 +187,25 @@ SELECT
 FROM visit
 ;
 
--- How many unique patients can be observed in the `dx` table? 
+-- 14) What visit category has the highest average number of visits per patient? 
+-- Hint: NULL is not a category
+-- Output dimensions: 2x4
+-- Output must contain columns:  `visit_category`, `person_count`, `visit_count`, `mean_visit_count`
+SELECT 
+visit_category
+,count(distinct(person_id)) as person_count
+,count(distinct(visit_id)) as visit_count
+,count(distinct(visit_id))/count(distinct(person_id)) as mean_visit_count
+FROM visit
+WHERE visit_category is not null
+GROUP BY visit_category 
+ORDER BY mean_visit_count desc
+;
+
+-- 15) How many unique diagnosis codes can be observed in the `dx` table? 
 -- Write the query that returns counts of unique values for each column
--- Requirements:
 -- Output dimensions: 1x6
--- Output must include columns named:
+-- Output must contain columns: 
 -- `dx_count`, `person_count`, `date_count`, `dx_code_count`, `dx_label_count`, `ipv_count`
 select
  count(distinct dx_id) as dx_count
@@ -127,33 +217,7 @@ select
 from dx
 ;
 
-
--- What is the average number of unique diagnoses (icd9_code) per patient in the `dx` table
--- (rounded down to the nearest integer)
--- Transitional questions (ones you must answer en route):
--- How many unique patients are in the `dx` table? 
--- How many unique diagnoses (codes) are in the `dx` table?
-SELECT 
- count(distinct(person_id)) as person_count
-,count(distinct(icd9_code)) as dx_count
-,count(distinct(icd9_code))/count(distinct(person_id)) as mean_dx
-FROM dx
-;
-
--- What is the average number of diagnoses (icd9_code) per patient in the `dx` table
--- (rounded down to the nearest integer)
--- Transitional questions (ones you must answer en route):
--- How many unique patients are in the `dx` table? 
--- How many diagnoses (icd9_code) are in the `dx` table?
-SELECT 
-count(distinct(dx_id)) as dx_count
-,count(distinct(person_id)) as person_count
-,count(distinct(dx_id))/count(distinct(person_id)) as mean_dx
-FROM dx
-;
-
-
--- Write the query that returns `icd9_description`s that have multiple `icd9_code`s. 
+-- 16) Write the query that returns unique `icd9_description`s that have multiple `icd9_code`s. 
 -- What value of `icd9_description` appears in the first row when sorted alphabetically?
 -- MUST use: HAVING
 -- must NOT use: WHERE, LEFT JOIN, INNER JOIN
@@ -169,198 +233,71 @@ having icd9_code_count > 1
 order by icd9_description asc
 ;
 
--- What visit category has the highest average number of visits per patient? 
--- NULL is not a category
-SELECT 
-visit_category
-,count(distinct(person_id)) as person_count
-,count(distinct(visit_id)) as visit_count
-,count(distinct(visit_id))/count(distinct(person_id)) as mean_visit_count
-FROM visit
-WHERE visit_category is not null
-GROUP BY visit_category 
-ORDER BY mean_visit_count desc
-;
 
--- PART III
-
-
-
--- List ids of patients present in ALL THREE tables and sort them in descending order
--- What value appears in the first row?
--- MUST use: INNER JOIN
--- must NOT use: LEFT JOIN
--- Output dimensions: ?x1
-SELECT
-  distinct
-  p.person_id
-  -- count(distinct p.person_id) as person_count
-FROM patient p
-  inner join dx    as d on p.person_id = d.person_id
-  inner join visit as v on p.person_id = v.person_id
-ORDER BY p.person_id desc
-;
- 
-
--- Among  patients in `dx` table who are missing from `visit` table, what person_id number is the smallest?
--- Hint 1: Write the query that lists patients present in `dx` but NOT in `visit` table
--- Hint 2: Sort by person_id found in the `visit` table
--- Output dimensions: ?x2
--- Output must include columns named: `person_id_dx`, `person_id_visit`
-select 
-distinct dx.person_id as person_id_dx
-, visit.person_id as person_id_visit
-from dx
-left join visit on dx.person_id = visit.person_id
-order by person_id_visit
-;
-
-
--- Among patients in `visit` table who are missing from `dx` table, what person_id number is the smallest?
--- Hint 1: Write the query that lists patients present in `visit` but NOT in `dx` table
--- Hint 2: Sort by person_id found in the `dx` table
--- Output dimensions: ?x2
--- Output must include columns named: `person_id_visit`, `person_id_dx`
-select 
-distinct visit.person_id as person_id_visit
-, dx.person_id as person_id_dx
-from visit
-left join dx on visit.person_id = dx.person_id
-order by person_id_dx
-;
-
--- Among patients missing from both `dx` and `visit` tables, what person_id number is the smallest?
-SELECT
-  distinct patient.person_id, dx.dx_id, visit.visit_id
-  --count(distinct patient.person_id) as person_count
-FROM patient 
-  left  join dx    on patient.person_id = dx.person_id
-  left  join visit on patient.person_id = visit.person_id
-WHERE 
-  dx.dx_id   is null
-  and
-  visit.visit_id is null
-ORDER BY patient.person_id asc
-;
-
--- Among patients present in all three tables, what person_id number is the smallest?
-SELECT
-  distinct patient.person_id as person_id_patient,
-  dx.person_id as person_id_dx, visit.person_id as person_id_visit
-  --count(distinct patient.person_id) as person_count
-FROM patient 
-  inner  join dx    on patient.person_id = dx.person_id
-  inner  join visit on patient.person_id = visit.person_id
-ORDER BY patient.person_id asc
-;
-
-
--- PART III
-
--- 1) How many more patients are in `patient` table than in the `dx` table?
--- Requirements:
--- Must use: count(), distinct()
+-- 17) What is the average number of unique diagnoses (icd9_code) per patient in the `dx` table
+-- (rounded down to the nearest integer)
+-- Helper questions:
+-- How many unique patients are in the `dx` table? 
+-- How many unique diagnoses (codes) are in the `dx` table?
 -- Output dimensions: 1x3
--- Output must contain columns: n_person_patient, n_person_dx, n_diff
+-- Output must contain columns:  `dx_count`,`person_count`, `mean_unique_dx`
 SELECT 
-count(distinct(a.person_id)) as person_count_patient,
-count(distinct(b.person_id)) as person_count_dx,
-abs(count(distinct(b.person_id)) - count(distinct(a.person_id))) as difference_count
-FROM patient as a
-LEFT join dx as b
+ count(distinct(icd9_code)) as dx_count
+,count(distinct(person_id)) as person_count
+,count(distinct(icd9_code))/count(distinct(person_id)) as mean_unique_dx
+FROM dx
 ;
 
-
--- 2) list person_id that are present in both `patient` and `dx` tables 
---    and sort them descending order. What value appears in the first row? 
--- Requirements:
--- Must use: AS, ON
--- Output dimensions: ?x1
--- Output must contain columns: person_id
-SELECT distinct(patient.person_id) as person_id
-FROM patient
-inner join dx
-ON patient.person_id = dx.person_id
-ORDER BY person_id desc
-;
-
--- 3) list person_id that are present in `patient` but are missing in `dx` table 
---    and sort them descending order. What value appears in the first row? 
-
+-- 18) What is the average number of non-unique diagnoses (icd9_code) per patient in the `dx` table
+-- (rounded down to the nearest integer)
+-- Transitional questions (ones you must answer en route):
+-- How many unique patients are in the `dx` table? 
+-- How many non-unique diagnoses (icd9_code) are in the `dx` table?
+-- Output dimensions: 1x3
+-- Output must contain columns:   `dx_count`,`person_count`, `mean_dx`
 SELECT 
-   distinct(patient.person_id) as person_id_patient
-   , dx.person_id as person_id_dx
-FROM patient
-left join dx
-ON patient.person_id = dx.person_id
-ORDER BY person_id_patient desc
+count(distinct(dx_id)) as dx_count
+,count(distinct(person_id)) as person_count
+,count(distinct(dx_id))/count(distinct(person_id)) as mean_dx
+FROM dx
 ;
-
--- Create a table listing the 
-
--- 3) list person_id that are present in `dx` but are missing in `patient` table 
---    and sort them descending order. What value appears in the first row? 
-
-
--- 1) List the inpa
-
-SELECT distinct(dx.person_id) as person_id
-FROM dx 
-LEFT JOIN patient
-ON dx.person_id = patient.person_id
-WHERE dx.inpatient_visit = 1 
-ORDER BY person_id DESC
-;
-
 
 
 
 -- PART IV
+-- 19) How many NULL values does the column `race` contain? 
 -- Write the query that counts the number of NULL values  in each column of the `patient` table
--- How many NULL values does the column `race` contain? 
--- Requirements:
 -- Must use: SUM, CASE WHEN, NULL
 -- Must NOT use: COUNT, 
 -- Output dimensions: 1x5
--- Output must contain columns: n_null_person_id, n_null_dob, n_null_gender, n_null_race, n_null_ethnicity
+-- Output must contain columns:
+-- `person_id_null_count`,`dob_null_count`,`gender_null_count`,`race_null_count`,`ethnicity_null_count`
+
 SELECT 
-   sum( case when person_id is NULL then 1 else 0 end)  as n_null_person_id
-  ,sum( case when dob       is NULL then 1 else 0 end)  as n_null_dob
-  ,sum( case when gender    is NULL then 1 else 0 end)  as n_null_gender
-  ,sum( case when race      is NULL then 1 else 0 end)  as n_null_race
-  ,sum( case when ethnicity is NULL then 1 else 0 end)  as n_null_ethnicity
+   sum( case when person_id is NULL then 1 else 0 end)  as person_id_null_count
+  ,sum( case when dob       is NULL then 1 else 0 end)  as dob_null_count
+  ,sum( case when gender    is NULL then 1 else 0 end)  as gender_null_count
+  ,sum( case when race      is NULL then 1 else 0 end)  as race_null_count
+  ,sum( case when ethnicity is NULL then 1 else 0 end)  as ethnicity_null_count
 FROM patient
 ;
 
--- Write the query that counts the number of NULL values  in each column of the `dx` table
--- How many NULL values does the column `icd9_code` contain? 
--- Requirements:
--- Must use: SUM, CASE WHEN, NULL
--- Must NOT use: COUNT, 
--- Output dimensions: 1x6
-SELECT 
-   sum( case when dx_id            is NULL then 1 else 0 end) as n_null_dx_id
-  ,sum( case when person_id        is NULL then 1 else 0 end) as n_null_person_id  
-  ,sum( case when dx_date          is NULL then 1 else 0 end) as n_null_dx_date
-  ,sum( case when icd9_code        is NULL then 1 else 0 end) as n_null_icd9_code
-  ,sum( case when icd9_description is NULL then 1 else 0 end) as n_null_icd9_description
-  ,sum( case when inpatient_visit  is NULL then 1 else 0 end) as n_null_inpatient
-FROM dx
-;
 
+-- 20) How many NULL values does the column `visit_category` contain? 
 -- Write the query that counts the number of NULL values in each column of the `visit` table
--- How many NULL values does the column `visit_category` contain? 
--- Requirements:
 -- Must use: COUNT
 -- Must NOT use: SUM, CASE WHEN, NULL
 -- Output dimensions: 1x6
+-- Output must contain columns:
+-- `visit_id_null_count`,`person_id_null_count`,`visit_category_null_count`,`visit_date_null_count`,`provider_id_null_count`,`care_site_id_null_count`
+
 SELECT 
-    count(*) - count(visit_id)       as n_null_visit_id
-   ,count(*) - count(person_id)      as n_null_person_id
-   ,count(*) - count(visit_category) as n_null_visit_category
-   ,count(*) - count(visit_date)     as n_null_visit_date
-   ,count(*) - count(provider_id)    as n_null_provider_id
-   ,count(*) - count(care_site_id)   as n_null_care_site_id
+    count(*) - count(visit_id)       as visit_id_null_count
+   ,count(*) - count(person_id)      as person_id_null_count
+   ,count(*) - count(visit_category) as visit_category_null_count
+   ,count(*) - count(visit_date)     as visit_date_null_count
+   ,count(*) - count(provider_id)    as provider_id_null_count
+   ,count(*) - count(care_site_id)   as care_site_id_null_count
 FROM visit
 ;
 
